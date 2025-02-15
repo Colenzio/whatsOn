a0c84d131c4de75aada29aa3c5f1559acd0f0a89
Dave <davidcollins1991@gmail.com>
2025-02-16 07:33:24 +1100

    First commit

---
+++ server.js
@@ -0,0 +1,23 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 5001;
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/buigit statusld')));
+
+// API route example
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from the backend!' });
+});
+
+// Catch-all route to serve the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
