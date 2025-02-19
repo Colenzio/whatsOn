87e13261db2c8d5a1307c7c1eac89d66b6ee4323
Colenzio <davidcollins1991@gmail.com>
2025-02-20 10:17:43 +1100

    cleaning up more structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 const apiUrl = process.env.REACT_APP_API_URL
 
-function App() {
+const App = () => {
 
   const [message, setMessage] = useState('');
 
@@ -23,7 +23,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" test_id="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
