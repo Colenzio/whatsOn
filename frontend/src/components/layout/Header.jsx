f6f408747c2f6ba43c918a6b10b59928b939c490
Dave <davidcollins1991@gmail.com>
2025-05-17 13:56:39 +1000

    css changes to header

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,13 +1,31 @@
 import React from "react";
-import styles from "./Header.module.css"
+import "./Header.css"; // Import your CSS file for styling
 
 const Header = () => {
     return (
-    <div className={styles.title}>
-        <header className="header"><h1>whatsOn</h1></header>
-        </div>   
-        );
-  };
-  
+        <div className="text-4xl">
+            <header className="header">
+                <h1>whatsOn</h1>
+                <nav className="nav-links">
+                    <a href="/home" className="link">
+                        Home
+                    </a>
+                    <a href="/about" className="link">
+                        About
+                    </a>
+                    <a href="/services" className="link">
+                        Services
+                    </a>
+                    <a href="/contact" className="link">
+                        Contact
+                    </a>
+                    <a href="/blog" className="link">
+                        Blog
+                    </a>
+                </nav>
+            </header>
+        </div>
+    );
+};
+
 export default Header;
-  
